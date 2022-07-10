73b2c721ddfb648f73045f9a16b4e11d4cff41b7
tokkk <mease250@gmail.com>
2022-07-10 13:26:43 +0900

    scroll js add

--- js/custom.js
+++ js/custom.js
@@ -29,5 +29,34 @@
 $(function() {
   $('.main-nav a').on('click', function(event){
     $('#drawer-check').prop('checked', false);
+    });
   });
-  });
+
+function load_effect(){
+ let element = document.getElementsByClassName('load-fade');
+ if(!element) return;
+
+ for(let i = 0; i < element.length; i++){
+  element[i].classList.add('is-show');
+    }
+  }
+
+setTimeout(load_effect, 600);
+
+
+function scroll_effect() {
+  let element = document.getElementsByClassName('scroll-up');
+  if(!element) return;
+                      
+  let scrollY = window.pageYOffset;
+  let windowH = window.innerHeight;
+  let showTiming = 200; // 要素を表示するタイミング
+  for(var i = 0; i < element.length; i++) { 
+    let elemClientRect = element[i].getBoundingClientRect(); 
+    let elemY = scrollY + elemClientRect.top; 
+    if(scrollY > elemY - windowH + showTiming) {
+      element[i].classList.add('is-show');
+    }
+  }
+}
+window.addEventListener('scroll', scroll_effect); // スクロール時に実行
