5c526f92f885427626c65cad99492b2e0de06a7d
tokkk <mease250@gmail.com>
2022-07-11 10:30:22 +0900

    scroll js add

--- js/custom.js
+++ js/custom.js
@@ -33,22 +33,31 @@ $(function() {
   });
 
     // scroll
-const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-  for (let i = 0; i < smoothScrollTrigger.length; i++){
-    smoothScrollTrigger[i].addEventListener('click', (e) => {
-      e.preventDefault();
-      let href = smoothScrollTrigger[i].getAttribute('href');
-       let targetElement = document.getElementById(href.replace('#', ''));
-      const rect = targetElement.getBoundingClientRect().top;
-      const offset = window.pageYOffset;
-      const gap = 100;
-      const target = rect + offset - gap;
-      window.scrollTo({
-        top: target,
-        behavior: 'smooth',
-      });
-    });
-  }
+$(function () {
+	var headerHight = $("header").height() + 50;
+
+	$('a[href^="#"]').click(function(e) {
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top - headerHight;
+
+		$.when(
+			$("html, body").animate({
+				scrollTop: position
+			}, 400, "swing"),
+			e.preventDefault(),
+		).done(function() {
+ 			var diff = target.offset().top - headerHight;
+ 			if (diff === position) {
+			} else {
+				$("html, body").animate({
+				scrollTop: diff
+				}, 10, "swing");
+			}
+		});
+	});
+});
+
 
 function load_effect(){
  let element = document.getElementsByClassName('load-fade');
